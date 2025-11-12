e5b884cf0e0ed4e8c7bd74ad84215b9f0a495e0d
joankixbe <joannakikaxb@gmail.com>
2025-11-12 10:55:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('successMessage').classList.remove('hidden');
+    this.reset();
+});
